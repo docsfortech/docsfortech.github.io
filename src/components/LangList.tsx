2de9f2dd54b208befb7c814849773806acfa8347
Ali Bouhrouche <ali@iev.ma>
2024-03-25 13:26:55 +0000

    Add lua

--- src/components/LangList.tsx
+++ src/components/LangList.tsx
@@ -1,32 +1,43 @@
 import {
-    Calculator,
-    Calendar,
-    CreditCard,
-    Settings,
-    Smile,
-    User,
-} from "lucide-react"
-
-import {
     Command,
     CommandEmpty,
     CommandGroup,
     CommandInput,
     CommandItem,
     CommandList,
+    CommandSeparator,
 } from "@/components/ui/command"
 import { Languages } from "./Languages"
 
-function navigateTo(value: string) {
-    location.href = `/${value.toLowerCase()}`
+function navigateToLang(key: string) {
+    const item = Languages[key];
+    if (item === undefined) return
+    if (item.url !== undefined) {
+        location.href = item.url
+        return
+    }
+    location.href = `/${key}`
 }
 
 export function LangList() {
     return (
         <Command className="rounded-lg border shadow-md">
-            <CommandInput placeholder="Search for languages & technologies..." />
+            <CommandInput placeholder="Search for languages & technologies..." autoFocus />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Languages">
+                    {Object.entries(Languages).map(([key, value]) => (
+                        <CommandItem
+                            key={key}
+                            value={key}
+                            title={value.title}
+                            onSelect={navigateToLang}
+                        >
+                            {value.icon}
+                            <span>{value.title}</span>
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
             </CommandList>
         </Command>
     )
